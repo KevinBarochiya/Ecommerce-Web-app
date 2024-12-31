2d2d5440442e891d16875ec6dba3a9ceb20aa70a
KevinBarochiya <aknpb0160b@gmail.com>
2024-12-31 17:59:37 +0530

    final4

--- config/mongoose-connection.js
+++ config/mongoose-connection.js
@@ -1,14 +1,13 @@
-const mongoose = require("mongoose");
-const config=require("config");
-const dbgr= require("debug")("development:mongoose");
+const mongoose = require('mongoose');
 
-mongoose
-.connect(`${process.env.MONGODB_URI}/mega`)
-.then(function(){
-    dbgr("running");
+// Replace <your_database_name> with your actual database name
+const mongoURI = process.env.MONGO_URI || 'mongodb+srv://myone:Kevin123@cluster0.cbj7p.mongodb.net/mega?ssl=true&replicaSet=atlas-142tsx-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 })
-.catch(function(err){
-    console.log(err);
-});
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
-module.exports= mongoose.connection;
+module.exports = mongoose.connection;
