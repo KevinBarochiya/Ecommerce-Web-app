58a4ec87392a276a736c5e8ba6fed4703e3b9476
KevinBarochiya <aknpb0160b@gmail.com>
2025-01-01 10:26:59 +0530

    final6

--- routes/ownersrouter.js
+++ routes/ownersrouter.js
@@ -1,24 +1,40 @@
 const express= require("express");
 const router=express.Router();
 const ownermodel=require("../models/ownermodel");
-
-if(process.env.NODE_ENV === "development"){
-    router.post("/create",async function(req,res){
-       let o=await ownermodel.find();
-       if(o.length>0){
-        return res.status(500).send("you not permition to create owner");
-       }
-
-       let {fullname,email,password}=req.body;
-       let createdowner= await ownermodel.create({
-        fullname,
-        email,
-        password,
-       });
-       res.status(201).send(createdowner);
-       res.send('we can create new owner');
+const bcrypt=require("bcryptjs");
+    router.get("/create",function(req,res){
+        res.render("owner-login");
     });
-}
+
+    router.post("/create",async function(req,res){
+    let o=await ownermodel.find();
+    if(o.length===0){
+       let {fullname,email,password}=req.body;
+       const hashedPassword = await bcrypt.hash(password, 10);
+       const createdOwner = await ownermodel.create({
+           email,
+           password: hashedPassword,
+       });
+       res.cookie("owner_id", createdOwner._id, { httpOnly: true });
+       res.redirect("/owners/admin");
+    }
+    if(o.length===1){
+        const { email, password } = req.body;
+        const owner = await ownermodel.findOne({ email });
+        if(!owner){
+            return res.send("Something went wrong");
+        }
+        const isMatch = await bcrypt.compare(password, owner.password);
+        if (!isMatch) {
+            return res.send("Something went wrong");
+        }
+        res.cookie("owner_id", owner._id, { httpOnly: true });
+        res.redirect("/owners/admin");
+    }
+    if(o.length>1){
+        res.send("Only one owner is allowed");
+    }
+    });
 
 router.get("/admin", function (req, res) {
     const success = req.flash("success"); 
